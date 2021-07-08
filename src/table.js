f5de42d365d60c80ffd4ce9bd5d08eab3a96127a
Daniel Brain <daniel@brainblocks.io>
2021-07-08 15:21:55 +0100

    Initial table

---
+++ src/table.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    thing1: `thing1 ${k}`,
+    thing2: `thing2 ${k}`,
+    thing3: `thing3 ${k}`,
+    thing4: `thing4 ${k}`,
+    thing5: `thing5 ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? "lightgreen" : "grey",
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getTableStyle = isDraggingOver => ({
+  width: '100%'
+});
+
+export class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(10)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  // Normally you would want to split things out into separate components.
+  // But in this example everything is just done in one place for simplicity
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <table
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getTableStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <tr
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      <td>
+                        {item.thing1}
+                      </td>
+                      <td>
+                        {item.thing2}
+                      </td>
+                      <td>
+                        {item.thing3}
+                      </td>
+                      <td>
+                        {item.thing4}
+                      </td>
+                      <td>
+                        {item.thing5}
+                      </td>
+                    </tr>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </table>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+}
